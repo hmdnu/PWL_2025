ef296fcd423c768ab1d2ac8fc739c203e4c471cf
hmdnu <daniuyan71@gmail.com>
2025-03-25 16:30:26 +0700

    feat: praktikum 2

--- week5/jobsheet5.2/resources/views/layouts/template.blade.php
+++ week5/jobsheet5.2/resources/views/layouts/template.blade.php
@@ -9,9 +9,12 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    {{-- <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    {{-- <link rel="stylesheet" href="../../dist/css/adminlte.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -24,7 +27,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ assets('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">PWL Starter Code</span>
             </a>
